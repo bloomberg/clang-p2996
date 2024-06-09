a2aead445e68e5dad89999ae33989b9714e3ad3f
Valentyn Yukhymenko <valentin.yukhymenko@gmail.com>
2024-06-09 18:22:05 +0100

    test: add more test cases
    Signed-off-by: Valentyn Yukhymenko <valentin.yukhymenko@gmail.com>

--- clang/test/Reflection/ambig-reflect-expr-parsing.cpp
+++ clang/test/Reflection/ambig-reflect-expr-parsing.cpp
@@ -34,9 +34,30 @@ void func(MyEnum && x) { // ok
   // expected-warning@-1 {{token '&' binds left to type 'T &'; did you mean '(^T) &'?}}
   // expected-error@-2 {{expected ';' at end of declaration}}
 
-  constexpr auto test_ref_type_0 = ^int &; // ok
-  constexpr auto test_ref_type_1 = ^int &&; // ok
-  constexpr auto test_ref_type_2 = (^int &); // ok
-  constexpr auto test_ref_type_3 = (^int &&); // ok
+  constexpr bool test_comparison_7 = ^MyEnum& == reflValue; // ok
+  constexpr bool test_comparison_8 = ^MyEnum& != reflValue; // ok
+
+  constexpr bool test_comparison_9 = ^MyEnum& < reflValue;
+  // expected-error@-1 {{invalid operands to binary expression ('meta::info' and 'const meta::info')}}
+  constexpr bool test_comparison_10 = ^MyEnum& > reflValue;
+  // expected-error@-1 {{invalid operands to binary expression ('meta::info' and 'const meta::info')}}
+  constexpr bool test_comparison_11 = ^MyEnum& <= reflValue;
+  // expected-error@-1 {{invalid operands to binary expression ('meta::info' and 'const meta::info')}}
+  constexpr bool test_comparison_12 = ^MyEnum& >= reflValue;
+  // expected-error@-1 {{invalid operands to binary expression ('meta::info' and 'const meta::info')}}
+
+  constexpr auto test_comparison_13 = reflValue != ^MyEnum&& || true; // ok
+  constexpr auto test_comparison_14 = reflValue != ^MyEnum&  | true; // ok
+
+  constexpr auto test_ref_type_0 = ^MyEnum &; // ok
+  constexpr auto test_ref_type_1 = ^MyEnum &&; // ok
+  constexpr auto test_ref_type_2 = (^MyEnum &); // ok
+  constexpr auto test_ref_type_3 = (^MyEnum &&); // ok
+
+  constexpr auto ternary_comparison_test_0 = reflValue == ^MyEnum& ? 1 : 0; // ok
+  constexpr auto ternary_comparison_test_1 = (2 > 0) ? ^MyEnum& : ^MyEnum; // ok
+
+  constexpr auto test_init_0 {^MyEnum&}; // ok
+  constexpr auto test_type_id_0 = ^MyEnum&(*)(bool); // ok
 }
 
