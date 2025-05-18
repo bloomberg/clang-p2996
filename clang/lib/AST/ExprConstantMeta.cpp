bf4338bcd756c5e07fb8b77f18f89ed58d1aa18b
Barry Revzin <barry.revzin@gmail.com>
2025-05-17 21:46:35 -0500

    Fixing extract from array

--- clang/lib/AST/ExprConstantMeta.cpp
+++ clang/lib/AST/ExprConstantMeta.cpp
@@ -3098,13 +3098,19 @@ bool extract(APValue &Result, ASTContext &C, MetaActions &Meta,
                                           Range.getBegin(), ResultTy, VK_LValue,
                                           Decl, nullptr);
       } else if (auto *ArrTy = dyn_cast<ArrayType>(Decl->getType())) {
-        QualType PtrTy = C.getPointerType(ArrTy->getElementType());
+        QualType Elt = ArrTy->getElementType();
+        if (auto *VD = dyn_cast<VarDecl>(Decl)) {
+          if (VD->isConstexpr()) {
+            Elt.addConst();
+          }
+        }
+
+        bool IsValid = RawResultTy->isPointerType() && RawResultTy->getPointeeType().isAtLeastAsQualifiedAs(Elt, C);
 
         ReturnsLValue = true;
-        if (RawResultTy.getCanonicalType().getTypePtr() !=
-            PtrTy.getCanonicalType().getTypePtr())
+        if (!IsValid)
           return Diagnoser(Range.getBegin(), diag::metafn_extract_type_mismatch)
-              << 1 << PtrTy << 1 << ResultTy << Range;
+              << 1 << C.getPointerType(Elt) << 1 << ResultTy << Range;
 
         NestedNameSpecifierLocBuilder NNSLocBuilder;
         if (auto *ParentClsDecl = dyn_cast_or_null<CXXRecordDecl>(
